48b1d87d88514c61762d21d63273acf8becbd80c
MitulPanchal <imitulpanchal@gmail.com>
2018-07-19 20:55:07 +0530

    - Delete Worked

--- todos/urls.py
+++ todos/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^details/(?P<id>\w{0,50})/$', views.details, name='details'),
-    url(r'^add/$', views.add, name='add')
+    url(r'^add/$', views.add, name='add'),
+    url(r'^details/(?P<id>\w{0,50})/delete$', views.delete, name='delete')
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import DeleteView
+
 from .models import Todo
 
 # Create your views here.
@@ -27,3 +30,15 @@ def add(request):
         return redirect('/')
     else:
         return render(request, 'add.html')
+
+def delete(request, id):
+    if(request.method == 'POST'):
+        todo = Todo.objects.get(id = id).delete()
+        temp_todo = Todo.objects.all()
+        context = {
+            'todo': todo,
+            'temp_todo': temp_todo
+        }
+        return redirect('/')
+    else:
+        return render(request, 'details.html')
